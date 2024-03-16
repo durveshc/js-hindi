2e1106d0f2a84f4008a6d5625f79dc00a82733ce
Durvesh Choudhary <84569729+durveshc@users.noreply.github.com>
2024-03-16 12:16:03 +0000

    let var const done with comment

---
+++ .devcontainer/01_basics/variables.js
@@ -0,0 +1,12 @@
+const accountId = 12345
+let accountemail = "abc@gmail.com"
+var accountPassword = "hello"
+accountcity = "Jaipur"
+let accountstate;
+
+ console.log(accountId);
+ 
+ accountemail = "xyz@gmail.com"
+ accountPassword = "hi and bye"
+ accountcity = "Delhi"
+ console.table([accountId, accountemail, accountPassword, accountcity, accountstate]);

c258bc30c252d3fe396da04ca0b1e91eddf3a711
Durvesh Choudhary <84569729+durveshc@users.noreply.github.com>
2024-03-17 12:02:29 +0000

    datatype done

---
+++ .devcontainer/01_basics/datatypes.js
@@ -0,0 +1,21 @@
+"use strict;" //treat js code 
+
+// alert(3 + 3) we are using nodejs not browser thats why it gave an error.
+// console.log(3 + 3);
+// console.log("Hello")
+
+
+// types of datatypes
+let name = "durvesh" //string
+let age = 23 //integer
+let isLoggedIn = true //boolean
+
+//string => written in "" or ''
+//number => 
+//boolean => true or false
+//undefined => value is not assign yet
+//null => empty 
+//bigint => use for big number like in trading or stocks
+//symbol => use for unqiueness mostly use in react 
+
+console.log(typeof isLoggedIn);
